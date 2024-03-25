e553bb1f45ffc6c039a8221c83c9cd75f634f397
Максим Красовский <maksimkrasovskiy@Air-Maksim.Dlink>
2024-03-25 21:14:46 +0300

    HW lesson1

---
+++ lesson1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Message from './components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text='Здесь должен быть текст!'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson1/src/components/Message.jsx
@@ -0,0 +1,9 @@
+function Message({text}) {
+    return ( 
+        <div>
+            <p className='text'>{text}</p> 
+        </div>
+    );
+}
+
+export default Message;
